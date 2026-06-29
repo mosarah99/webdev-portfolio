7d97267cf62b81f02053664f882341278e062cd5
Sadat R. <sadatrahman001@gmail.com>
2026-06-29 22:49:26 +0600

    correcting the GMaps iframe display

--- src/pages/Contact/Contact.component.tsx
+++ src/pages/Contact/Contact.component.tsx
@@ -21,6 +21,7 @@ export const ContactsPage = () => {
             }}
           >
             <Box
+            display={'block'}
               component='iframe'
               src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d160600.64768221346!2d-114.25263613551553!3d51.02750740792613!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x537170039f843fd5%3A0x266d3bb1b652b63a!2sCalgary%2C%20AB%2C%20Canada!5e0!3m2!1sen!2sbd!4v1782748259671!5m2!1sen!2sbd'
               width='100%'
