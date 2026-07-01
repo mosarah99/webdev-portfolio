7744015f61719d3c80f8774563ee8b02f275ecde
Sadat R. <sadatrahman001@gmail.com>
2026-07-01 19:35:13 +0600

    implemented LinkedIn and GH iframe
    As a visitor, I want to see an embedded LinkedIn frame because I want to see your professional updates.
    
    As a visitor, I want to see an embedded GitHub frame because I want to see your coding consistency.

--- src/pages/Contact/Contact.component.tsx
+++ src/pages/Contact/Contact.component.tsx
@@ -1,8 +1,33 @@
-import { Box, Card, Container } from '@mui/material';
+import { Box, Card, CardContent, Container } from '@mui/material';
 import ContactCard from '../../components/Cards/ContactCard/ContactCard.component';
 import SectionHeader from '../../components/SectionHeader/SectionHeader.component';
+import { useEffect, useState } from 'react';
+import Markdown from 'react-markdown';
+import rehypeRaw from 'rehype-raw';
 
 export const ContactsPage = () => {
+  const [ghReadme, setGhReadme] = useState<string>('# Loading REAME...');
+
+  useEffect(() => {
+    const fetchGhReadme = async () => {
+      try {
+        const response = await fetch(
+          'https://raw.githubusercontent.com/mosarah99/mosarah99/main/README.md',
+        );
+        if (!response.ok) {
+          throw new Error('Failed to fetch README.md');
+        }
+        const readmeText = await response.text();
+        setGhReadme(readmeText);
+      } catch (error) {
+        console.error('Error fetching README.md:', error);
+        setGhReadme('# Failed to load README.md');
+      }
+    };
+
+    fetchGhReadme();
+  }, []);
+
   return (
     <Box>
       <section>
@@ -13,7 +38,61 @@ export const ContactsPage = () => {
       </section>
       <section>
         <Container maxWidth='lg'>
-          <SectionHeader title='Where to find me' />
+          <SectionHeader
+            pretitle='Professional presence on'
+            title='LinkedIn'
+          />
+          <Box
+            component='iframe'
+            src='https://widgets.sociablekit.com/linkedin-profile-posts/iframe/25693876'
+            sx={{
+              width: '100%',
+              border: 0,
+              height: '20rem',
+              maxHeight: '800px',
+            }}
+          />
+        </Container>
+      </section>
+      <section>
+        <Container maxWidth='lg'>
+          <SectionHeader
+            pretitle='Having fun on'
+            title='GitHub'
+          />
+          <Card
+            variant='outlined'
+            sx={{
+              'backgroundColor': '#0d1117',
+              'p,h1, h2,h3,h4,h5,h6': {
+                color: '#c9d1d9',
+              },
+              'a': {
+                color: '#58a6ff',
+              },
+            }}
+          >
+            <CardContent>
+              <Box
+                sx={{
+                  overflow: 'auto',
+                  img: {
+                    maxWidth: '100%',
+                  },
+                }}
+              >
+                <Markdown rehypePlugins={[rehypeRaw]}>{ghReadme}</Markdown>
+              </Box>
+            </CardContent>
+          </Card>
+        </Container>
+      </section>
+      <section>
+        <Container maxWidth='lg'>
+          <SectionHeader
+            pretitle='Locate me on'
+            title='Google Maps'
+          />
           <Card
             sx={{
               margin: 0,
@@ -21,7 +100,7 @@ export const ContactsPage = () => {
             }}
           >
             <Box
-            display={'block'}
+              display={'block'}
               component='iframe'
               src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d160600.64768221346!2d-114.25263613551553!3d51.02750740792613!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x537170039f843fd5%3A0x266d3bb1b652b63a!2sCalgary%2C%20AB%2C%20Canada!5e0!3m2!1sen!2sbd!4v1782748259671!5m2!1sen!2sbd'
               width='100%'
--- src/pages/Projects/Projects.component.tsx
+++ src/pages/Projects/Projects.component.tsx
@@ -4,23 +4,177 @@ import {
   ButtonGroup,
   Card,
   CardActions,
+  CardContent,
   CardHeader,
   CardMedia,
   Container,
   Grid,
+  IconButton,
+  Stack,
+  Typography,
 } from '@mui/material';
 import type React from 'react';
 import SectionHeader from '../../components/SectionHeader/SectionHeader.component';
 import FeaturedProject from '../../components/FeaturedProject/FeaturedProject.component';
 import * as uuid from 'uuid';
+import LaunchIcon from '@mui/icons-material/Launch';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 import './Projects.style.css';
 
 import { projects as projectsList } from '../../assets/projectsList';
 import { useNavigate } from 'react-router';
+import { useState } from 'react';
+
+const ProjectGridCard: React.FC<{
+  project: (typeof projectsList)[number];
+  projectDetailsPageURL?: string;
+}> = ({ project, projectDetailsPageURL }) => {
+  const navigate = useNavigate();
+
+  const handleViewDetailsClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    navigate(projectDetailsPageURL || `/projects/${project.id}`);
+  };
+
+  return (
+    <Grid size={{ xs: 12, md: 6 }}>
+      <Card>
+        <CardMedia
+          image={project.image}
+          component={'img'}
+          alt={`${project.title} image`}
+          sx={{
+            objectFit: 'cover',
+            aspectRatio: 1,
+            boxShadow: '5px 5px 5px #eee',
+          }}
+        />
+        <Box>
+          <CardHeader
+            title={project.title}
+            subheader={project.description}
+          />
+          <CardActions>
+            <ButtonGroup
+              variant='outlined'
+              fullWidth
+            >
+              <Button
+                variant='outlined'
+                href={projectDetailsPageURL || `/projects/${project.id}`}
+                onClick={handleViewDetailsClick}
+              >
+                View details
+              </Button>
+              {!!project.githubLink && (
+                <Button
+                  variant='contained'
+                  href={project.githubLink}
+                  target='_blank'
+                  referrerPolicy='no-referrer'
+                >
+                  View on GitHub
+                </Button>
+              )}
+            </ButtonGroup>
+          </CardActions>
+        </Box>
+      </Card>
+    </Grid>
+  );
+};
+
+const ProjectListCard: React.FC<{
+  project: (typeof projectsList)[number];
+  projectDetailsPageURL?: string;
+}> = ({ project, projectDetailsPageURL }) => {
+  const navigate = useNavigate();
+
+  const handleViewDetailsClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    navigate(projectDetailsPageURL || `/projects/${project.id}`);
+  };
+
+  return (
+    <Grid size={{ xs: 12 }}>
+      <Card
+        sx={
+          {
+            // display: 'flex',
+            // flexDirection: 'row',
+            // alignItems: 'center',
+            // minHeight: '200px',
+          }
+        }
+      >
+        <Stack
+          flexDirection={'row'}
+          alignItems={'center'}
+        >
+          <CardMedia
+            image={project.image}
+            component={'img'}
+            alt={`${project.title} image`}
+            sx={{
+              objectFit: 'cover',
+              aspectRatio: 1,
+
+              width: '15rem',
+              maxWidth: '120px',
+            }}
+          />
+          <Box sx={{ flex: 1, margin: 1, marginLeft: 2 }}>
+            <Stack
+              flexDirection={'row'}
+              alignItems={'flex-start'}
+              justifyContent={'space-between'}
+            >
+              <CardHeader
+                title={project.title}
+                subheader={project.description}
+              />
+              <CardActions>
+                <ButtonGroup
+                  sx={{
+                    display: 'flex',
+                    flexDirection: {
+                      xs: 'column',
+                      md: 'row',
+                    },
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                  }}
+                >
+                  <IconButton
+                    // variant='outlined'
+                    // href={projectDetailsPageURL || `/projects/${project.id}`}
+                    onClick={handleViewDetailsClick}
+                  >
+                    <LaunchIcon />
+                  </IconButton>
+                  {!!project.githubLink && (
+                    <IconButton
+                      // variant='contained'
+                      href={project.githubLink}
+                      target='_blank'
+                      referrerPolicy='no-referrer'
+                    >
+                      <GitHubIcon />
+                    </IconButton>
+                  )}
+                </ButtonGroup>
+              </CardActions>
+            </Stack>
+          </Box>
+        </Stack>
+      </Card>
+    </Grid>
+  );
+};
 
 export const ProjectsPage: React.FC = () => {
-  const navigate = useNavigate();
+  const [viewMode, setViewMode] = useState<'grid' | 'list'>('grid');
 
   return (
     <Box>
@@ -70,64 +224,62 @@ export const ProjectsPage: React.FC = () => {
           pretitle='A bit more detailed'
           title='Projects List'
         />
+        <Container
+          maxWidth='lg'
+          sx={{ marginBottom: 2 }}
+        >
+          <Card variant='outlined'>
+            <CardContent>
+              <Stack
+                flexDirection={'row'}
+                alignItems={'center'}
+                justifyContent={'space-between'}
+              >
+                <Typography variant='button'>View</Typography>
+                <ButtonGroup
+                  variant='outlined'
+                  aria-label='outlined button group'
+                >
+                  <Button
+                    variant={viewMode === 'grid' ? 'contained' : 'outlined'}
+                    onClick={() => setViewMode('grid')}
+                  >
+                    Grid
+                  </Button>
+                  <Button
+                    variant={viewMode === 'list' ? 'contained' : 'outlined'}
+                    onClick={() => setViewMode('list')}
+                  >
+                    List
+                  </Button>
+                </ButtonGroup>
+              </Stack>
+            </CardContent>
+          </Card>
+        </Container>
         <Container maxWidth='lg'>
           <Grid
             container
             spacing={2}
           >
-            {projectsList.map((_project, _index) => (
-              <Grid
-                key={uuid.v7()}
-                size={{ xs: 12, md: 6 }}
-              >
-                <Card>
-                  <CardMedia
-                    image={_project.image}
-                    component={'img'}
-                    alt={`${_project.title} image`}
-                    sx={{
-                      objectFit: 'cover',
-                      aspectRatio: 1,
-                      boxShadow: '5px 5px 5px #eee',
-                    }}
+            {projectsList.map((_project, _index) => {
+              if (viewMode === 'grid')
+                return (
+                  <ProjectGridCard
+                    key={uuid.v7()}
+                    project={_project}
+                    projectDetailsPageURL={`/projects/${_project.id}`}
                   />
-                  <Box>
-                    <CardHeader
-                      title={_project.title}
-                      subheader={_project.description}
-                    />
-                    <CardActions>
-                      <ButtonGroup
-                        variant='outlined'
-                        fullWidth
-                      >
-                        <Button
-                          variant='outlined'
-                          onClick={() => {
-                            // setModalContent(project);
-                            // setModalOpen(true);
-
-                            navigate(`/projects/${_project.id}`);
-                          }}
-                        >
-                          View details
-                        </Button>
-                        {!!_project.githubLink && (
-                          <Button
-                            variant='contained'
-                            href={_project.githubLink}
-                            target='_blank'
-                            referrerPolicy='no-referrer'
-                          >
-                            View on GitHub
-                          </Button>
-                        )}
-                      </ButtonGroup>
-                    </CardActions>
-                  </Box>
-                </Card>
-              </Grid>
-            ))}
+                );
+              else
+                return (
+                  <ProjectListCard
+                    key={uuid.v7()}
+                    project={_project}
+                    projectDetailsPageURL={`/projects/${_project.id}`}
+                  />
+                );
+            })}
           </Grid>
         </Container>
       </section>
