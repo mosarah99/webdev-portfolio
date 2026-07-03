b3e7d8625dbceb3926b5dfaf5210c92e8551d77e
Sadat R. <sadatrahman001@gmail.com>
2026-07-03 07:16:46 +0600

    added blur effect to paper component

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,4 +1,5 @@
 import {
+  alpha,
   createTheme,
   type PaletteOptions,
   type Theme,
@@ -11,12 +12,57 @@ import themePalettes from './palettes';
  * Use color-presets instead of color palettes for more granular control over theme customization.
  */
 
-
 export const generateThemeOptions = (
   palette: PaletteOptions,
 ): ThemeOptions => ({
   palette: palette,
   components: {
+    MuiPaper: {
+      variants: [
+        {
+          props: (props) => true, // Apply this variant to all Paper components
+          style: ({ theme }) => ({
+            backgroundColor: alpha(theme.palette.background.paper, 0.4),
+            backdropFilter: 'blur(12px)',
+            WebkitBackdropFilter: 'blur(12px)',
+            border: `1px solid ${alpha(theme.palette.common.white, 0.2)}`,
+            boxShadow: theme.shadows[4],
+          }),
+        },
+      ],
+    },
+    // MuiButton: {
+    //   variants: [
+    //     {
+    //       props: { variant: 'outlined', color: 'primary' },
+    //       style: ({ theme }) => ({
+    //         'backgroundColor': alpha(theme.palette.primary.light, 0.4),
+    //         'backdropFilter': 'blur(12px)',
+    //         'WebkitBackdropFilter': 'blur(12px)',
+    //         'border': `1px solid ${alpha(theme.palette.primary.dark, 0.2)}`,
+    //         'boxShadow': theme.shadows[4],
+    //         ':hover': {
+    //           backgroundColor: alpha(theme.palette.primary.light, 0.6),
+    //         },
+    //         color: theme.palette.primary.contrastText,
+    //       }),
+    //     },
+    //     {
+    //       props: { variant: 'outlined', color: 'secondary' },
+    //       style: ({ theme }) => ({
+    //         'backgroundColor': alpha(theme.palette.secondary.light, 0.4),
+    //         'backdropFilter': 'blur(12px)',
+    //         'WebkitBackdropFilter': 'blur(12px)',
+    //         'border': `1px solid ${alpha(theme.palette.secondary.dark, 0.2)}`,
+    //         'boxShadow': theme.shadows[4],
+    //         ':hover': {
+    //           backgroundColor: alpha(theme.palette.secondary.light, 0.6),
+    //         },
+    //         color: theme.palette.secondary.contrastText,
+    //       }),
+    //     },
+    //   ],
+    // },
     MuiTooltip: {
       defaultProps: {
         arrow: true,
