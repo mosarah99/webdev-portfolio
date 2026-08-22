138ece52c9947ab3e8b8917b9470f246722aebc0
Sadat R. <sadatrahman001@gmail.com>
2026-08-22 17:08:36 +0600

    Memoized Router functions for perf imprv

--- src/components/Navigation/Router/Router.component.tsx
+++ src/components/Navigation/Router/Router.component.tsx
@@ -10,43 +10,53 @@ export interface Page {
 }
 
 interface RouterProps extends React.ComponentPropsWithoutRef<'div'> {
-  header?: React.ReactElement;
-  footer?: React.ReactElement;
   pages: Page[];
 }
 
-export const Router = ({ ...props }: RouterProps) => {
-  const { pages } = props;
-  if (pages.length === 0)
-    pages.push({ slug: '', component: <React.Fragment /> });
-  const defaultPage = pages.find((page) => page.defaultPage) ?? pages[0];
-  const errorPage = pages.find((page) => page.errorPage) ?? pages[0];
-  const pageMap = new Map(pages.map((page) => [page.slug, page]));
-  pageMap.set('index', defaultPage);
-  pageMap.set('default', defaultPage);
-  pageMap.set('defaultPage', defaultPage);
-  pageMap.set('error', errorPage);
-  pageMap.set('errorPage', errorPage);
-
-  const [page, _setPage] = useQueryState(
-    'p',
-    parseAsStringEnum(pages.map((page) => page.slug))
-      .withDefault(defaultPage.slug)
-      .withOptions({
-        history: 'push',
-        scroll: true,
-        clearOnDefault: true,
-      }),
+export const Router = ({ pages, ...props }: RouterProps) => {
+  const safePages: Page[] = useMemo(
+    () =>
+      pages.length > 0 ? pages : [{ slug: '', component: <React.Fragment /> }],
+    [pages],
+  );
+  const defaultPage = useMemo(
+    () => safePages.find((page) => page.defaultPage) ?? safePages[0],
+    [safePages],
+  );
+  const errorPage = useMemo(
+    () => safePages.find((page) => page.errorPage) ?? safePages[0],
+    [safePages],
+  );
+  const pageMap = useMemo(
+    () => new Map(safePages.map((page) => [page.slug, page])),
+    [safePages],
   );
 
-  const currentPage = useMemo(
-    () => pageMap.get(page)?.component ?? defaultPage.component,
-    [page],
-  );
+  const [page, setPage] = useQueryState('p', {
+    // defaultValue: defaultPage.slug,
+    history: 'push',
+    scroll: true,
+    clearOnDefault: true,
+  });
+
+  useEffect(() => {
+    console.log(`useEffect runs`);
+
+    if (page === defaultPage.slug || !page) {
+      setPage(null, { history: 'replace' });
+    }
+  }, [page, defaultPage.slug, setPage]);
+
+  const currentComponent = useMemo(() => {
+    console.log(`currentComponent updated`);
+
+    if (!page || page === defaultPage.slug) return defaultPage.component;
+    return pageMap.get(page)?.component ?? errorPage.component;
+  }, [page]);
 
   return (
     <div {...props}>
-      {currentPage}
+      {currentComponent}
       {props.children ?? <></>}
     </div>
   );
