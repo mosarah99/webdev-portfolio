e490b9155d47d9980413f767492d1faf7de15aa2
Sadat R. <sadatrahman001@gmail.com>
2026-09-22 12:39:27 +0600

    dev: About Me section on Homepage replaced by a Markdown text and component for extensibility

--- src/pages/Home/Home.component.tsx
+++ src/pages/Home/Home.component.tsx
@@ -21,6 +21,7 @@ import PrimarySection from '../../components/Section/PrimarySection/PrimarySecti
 import SecondarySection from '../../components/Section/SecondarySection/SecondarySection.component';
 import ContrastSection from '../../components/Section/ContrastSection/ContrastSection.component';
 import HeroSection from '../../templates/HeroSection/HeroSection.component';
+import MarkdownFancy from '../../components/MarkdownFancy/MarkdownFancy.component';
 
 export const Homepage: React.FC = () => {
   const theme = useTheme();
@@ -52,60 +53,16 @@ export const Homepage: React.FC = () => {
         />
 
         <Container maxWidth='lg'>
-          <Typography>
-            As a full-stack developer, I specialize in
-            building robust, scalable, and intuitive digital
-            solutions that bridge front-end elegance with
-            back-end strength. My technical foundation spans
-            a wide range of languages—including Java,
-            Kotlin, Python, C++, JavaScript, and
-            TypeScript—and is supported by extensive
-            experience with modern frameworks such as React,
-            Angular, Spring Boot, ASP.NET, and Node.js.
-            Whether it's architecting APIs, managing
-            databases (SQL or NoSQL), or deploying to AWS or
-            Azure, I approach every project with a deep
-            commitment to reliability, maintainability, and
-            performance.
-          </Typography>
-          <Typography>
-            In addition to my technical expertise, I've had
-            the privilege of contributing to meaningful
-            modernization projects during my time at the
-            Canada Revenue Agency, where I developed
-            internal tooling to streamline workflows and
-            increase team productivity. Similarly, at J.D.
-            Power, I played a key role in resolving
-            technical issues, upgrading infrastructure, and
-            helping onboard new hires—all of which sharpened
-            my ability to troubleshoot quickly and work
-            cross-functionally. These professional
-            experiences have not only refined my technical
-            approach but also instilled in me a strong sense
-            of ownership, accountability, and attention to
-            detail.
-          </Typography>
-          <Typography>
-            Moreover, I believe that strong collaboration
-            and communication are just as important as clean
-            code. I bring a team-first mindset, adaptability
-            across roles and environments, and a willingness
-            to learn and teach. This mindset has helped me
-            consistently deliver value in both solo and
-            team-based projects, and it's part of why I was
-            recognized on the President's Honour Roll at
-            Fanshawe College.
-          </Typography>
-          <Typography>
-            Ultimately, I see software development as both a
-            science and an art—a craft that combines logic,
-            creativity, and a bit of digital wizardry.
-            Whether I'm developing production-ready
-            features, automating internal tools, or
-            designing user-focused experiences, I strive to
-            create solutions that are not only functional
-            but thoughtfully built and future-ready.
-          </Typography>
+          <MarkdownFancy>
+            {`As a full-stack developer, I specialize in building robust, scalable, and intuitive digital solutions that bridge front-end elegance with back-end strength. My technical foundation spans a wide range of languages—including Java, Kotlin, Python, C++, JavaScript, and TypeScript—and is supported by extensive experience with modern frameworks such as React, Angular, Spring Boot, ASP.NET, and Node.js. Whether it's architecting APIs, managing databases (SQL or NoSQL), or deploying to AWS or Azure, I approach every project with a deep commitment to reliability, maintainability, and performance.
+
+In addition to my technical expertise, I've had the privilege of contributing to meaningful modernization projects during my time at the Canada Revenue Agency, where I developed internal tooling to streamline workflows and increase team productivity. Similarly, at J.D. Power, I played a key role in resolving technical issues, upgrading infrastructure, and helping onboard new hires—all of which sharpened my ability to troubleshoot quickly and work cross-functionally. These professional experiences have not only refined my technical approach but also instilled in me a strong sense of ownership, accountability, and attention to detail.
+
+Moreover, I believe that strong collaboration and communication are just as important as clean code. I bring a team-first mindset, adaptability across roles and environments, and a willingness to learn and teach. This mindset has helped me consistently deliver value in both solo and team-based projects, and it's part of why I was recognized on the President's Honour Roll at Fanshawe College.
+
+Ultimately, I see software development as both a science and an art—a craft that combines logic, creativity, and a bit of digital wizardry. Whether I'm developing production-ready features, automating internal tools, or designing user-focused experiences, I strive to create solutions that are not only functional but thoughtfully built and future-ready.
+`}
+          </MarkdownFancy>
         </Container>
         <Container maxWidth='lg'>
           <ButtonGroup
